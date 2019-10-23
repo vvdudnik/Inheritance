b7f4ce4bde4ae29978214831d2ca0b95a4b1e35d
Vladimir Dudnik <vvdudnik@gmail.com>
2019-10-23 23:26:20 +0300

    change1

---
+++ Inheritance/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Class1
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("Method1 from class1");
+        }
+
+        public virtual void Method2()
+        {
+            Console.WriteLine("Method2 from class1");
+        }
+    }
+
+    class Class2:Class1
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Method1 from class2");
+        }
+
+        public override void Method2()
+        {
+            Console.WriteLine("Method2 from class2");
+        }
+    }
+    class Class3:Class2
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Method1 from class3");
+        }
+
+        public virtual void Method2()
+        {
+            Console.WriteLine("Method2 from class3");
+        }
+    }
+    class Class4:Class3
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Method1 from class4");
+        }
+
+        public override void Method2()
+        {
+            Console.WriteLine("Method2 from class4");
+        }
+    }
+
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,6 +10,31 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            int[,] values =             { { 1, 2,3},
+                                        {4,5,6 }};
+
+            int l = values.Length;
+            int l1 = values.GetLength(0);
+            int l2 = values.GetLength(1);
+            Class1 c1 = new Class1();
+            Class1 c2 = new Class2();
+            Class1 c3 = new Class3();
+            Class1 c4 = new Class4();
+
+            c1.Method1();
+            c1.Method2();
+            Console.WriteLine();
+            c2.Method1();
+            c2.Method2();
+            Console.WriteLine();
+
+            c3.Method1();
+            c3.Method2();
+            Console.WriteLine();
+
+            c4.Method1();
+            c4.Method2();
+            Console.ReadLine();
         }
     }
 }
